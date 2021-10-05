a47b12bf9be8acd2fee03c1ea2dfcb11c531f54d
Kenneth Schackart <schackartk1@gmail.com>
2021-10-04 20:14:43 -0700

    add string arg test

--- 01_dna/tests/testthat/test_dna.R
+++ 01_dna/tests/testthat/test_dna.R
@@ -1,4 +1,7 @@
-PRG = "../../dna.R"
+PRG <- "../../dna.R"
+TEST1 <- c("../inputs/input1.txt", "1 2 3 4")
+TEST2 <- c("../inputs/input2.txt", "20 12 17 21")
+TEST3 <- c("../inputs/input3.txt", "196 231 237 246")
 
 run_cmd <- function(cmd) {
   system(cmd, intern = TRUE, ignore.stderr = TRUE)
@@ -18,4 +21,12 @@ test_that("Program prints usage", {
                  "usage")
   }
   
+})
+
+test_that("Runs with arg", {
+  for (test_set in list(TEST1, TEST2, TEST3)) {
+    dna <- readr::read_file(test_set[1])
+    expect_equal(run_cmd(stringr::str_glue("Rscript {PRG} {dna}")),
+                 test_set[2])
+  }
 })
