0a955c7bdb15e2eee478e13647edb97d0a782a30
Kenneth Schackart <schackartk1@gmail.com>
2021-10-04 20:15:15 -0700

    add readr requirement

--- requirements.R
+++ requirements.R
@@ -6,4 +6,5 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load("argparse",
                "lintr",
                "stringr",
+               "readr",
                "testthat")
