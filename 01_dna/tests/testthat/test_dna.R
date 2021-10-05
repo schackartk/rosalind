e382102bdacc3b7f05c952327002c9aae0ca41ad
Kenneth Schackart <schackartk1@gmail.com>
2021-10-04 20:23:36 -0700

    add final test

--- 01_dna/tests/testthat/test_dna.R
+++ 01_dna/tests/testthat/test_dna.R
@@ -7,14 +7,17 @@ run_cmd <- function(cmd) {
   system(cmd, intern = TRUE, ignore.stderr = TRUE)
 }
 
+# ---------------------------------------------------------------------------
 test_that("Program exists", {
   expect_true(file.exists(PRG))
 })
 
+# ---------------------------------------------------------------------------
 test_that("Program is lint free", {
   expect_silent(lintr::lint(PRG))
 })
 
+# ---------------------------------------------------------------------------
 test_that("Program prints usage", {
   for (flag in c("-h", "--help")) {
     expect_match(run_cmd(stringr::str_glue("Rscript {PRG} {flag}"))[1],
@@ -23,10 +26,20 @@ test_that("Program prints usage", {
   
 })
 
+# ---------------------------------------------------------------------------
 test_that("Runs with arg", {
   for (test_set in list(TEST1, TEST2, TEST3)) {
     dna <- readr::read_file(test_set[1])
     expect_equal(run_cmd(stringr::str_glue("Rscript {PRG} {dna}")),
+                 test_set[2])
+  }
+})
+
+# ---------------------------------------------------------------------------
+test_that("Runs with file", {
+  for (test_set in list(TEST1, TEST2, TEST3)) {
+    in_file <- test_set[1]
+    expect_equal(run_cmd(stringr::str_glue("Rscript {PRG} {in_file}")),
                  test_set[2])
   }
 })
